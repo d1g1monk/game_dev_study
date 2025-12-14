913443786a56abe85966af8616013dc8086e3b01
d1g1monk <c0kk0000000@gmail.com>
2025-12-14 17:06:10 +0300

    gui

--- code/title.gd
+++ code/title.gd
@@ -3,9 +3,13 @@ extends Control
 func _ready():
     mouse_filter = Control.MOUSE_FILTER_STOP  # or MOUSE_FILTER_STOP
 
+func _on_label_3_button_up() -> void:
+    print("Restarting")
+    get_tree().change_scene_to_file("res://scene/main.tscn")       
 
-
-        
+func _unhandled_input(event: InputEvent) -> void:
+    if event.is_action_released("space"):
+        _on_label_3_button_up()       
         
 #func _process(_delta: float) -> void:
 #    if (
@@ -13,8 +17,3 @@ func _ready():
 #        Input.is_action_just_pressed("space")
 #        ):
 #        get_tree().change_scene_to_file("res://scene/main.tscn")
-
-
-func _on_label_3_button_up() -> void:
-    print("Restarting")
-    get_tree().change_scene_to_file("res://scene/main.tscn")       
